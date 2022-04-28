bbb09d197acb8f81940df0f58186d0d23b7496e5
kylinhunter <250917500@qq.com>
2022-04-29 01:36:18 +0800

    init

---
+++ k-dic-core/src/main/java/com/kylinhunter/nlp/dic/core/dictionary/group/bean/DictionaryNode.java
@@ -0,0 +1,13 @@
+package com.kylinhunter.nlp.dic.core.dictionary.group.bean;
+
+import com.kylinhunter.nlp.dic.core.analyzer.bean.Words;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+public class DictionaryNode {
+    protected HitMode hitMode;
+    protected String keyword;
+}
